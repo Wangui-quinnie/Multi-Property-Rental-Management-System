7332c0120946b01b71a6504a5c3117d593dfbbaa
wangui-quinnie <w.wangui@ymail.com>
2026-07-10 02:42:43 +0300

    Create property API with CRUD endpoints

--- apps/properties/views.py
+++ apps/properties/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from apps.core.api.pagination import DefaultPagination
+from apps.core.api.responses import success_response
+
+from .models import Property
+from .serializers import PropertySerializer
+
+
+class PropertyViewSet(ModelViewSet):
+
+    serializer_class = PropertySerializer
+
+    queryset = Property.objects.all()
+
+    pagination_class = DefaultPagination
--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
+
     path("admin/", admin.site.urls),
+    path(
+        "api/auth/",
+        include("apps.accounts.urls"),
+    ),
 
-    path("api/auth/", include("apps.accounts.urls")),
+    path(
+        "api/properties/",
+        include("apps.properties.urls"),
+    ),
 
-    path("reports/", include("apps.reports.urls")),
+    path(
+        "reports/",
+        include("apps.reports.urls"),
+    ),
+
 ]
